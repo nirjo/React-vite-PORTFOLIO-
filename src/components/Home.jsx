4543c7e63af4e699f0cc448004254d0b6dcd1024
Nirmalraj <nirmalraj.joseph@twilightsoftwares.com>
2023-03-15 13:48:19 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import profileImg from '../assets/profileImg.png'
+import { FaBeer, } from 'react-icons/fa';
+// import { FaTwitter,FaDribble,FaBehance } from 'react-icons/fa'
+import './home.css'
+
+const Home = () => {
+  return (
+<section>
+    <div className="home" >
+        <div className="home__contianer ">
+            <p className="home__subtitle text-cs">
+            Hello , <span> My name Is</span>
+
+            </p>
+            <h1 className='home__title text -cs'>
+            <span>NirmalRaj </span>
+            Joseph
+            </h1>
+<p className='home__job'>
+    <span className='text-cs'>I Am </span><b>FrontEnd Developer </b> 
+</p>
+<p className='home__text'>
+ From Pondicherry, india. I have rich experience in frontend design and development , also im good at nodejs backend.
+
+</p>
+
+<div className='home__socials'>
+    <a href=''className='home__social-link'>
+    <FaBeer />
+    </a>
+    <a href=''className='home__social-link'>
+    <FaBeer />
+    </a>
+    <a href=''className='home__social-link'>
+    <FaBeer />
+    </a>
+</div>
+<div className='home__btns'>
+    <a href='' className='btn text-cs'>
+       <button >
+       Download CV
+       </button>
+       </a>
+
+    <a href='' className='hero__link text-cs'>
+    <button >
+        my Skills
+        </button>
+    </a>
+</div>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default Home
