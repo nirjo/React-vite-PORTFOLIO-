1084c75f16c2f05be84456ba7ccb6461fcac3e9f
Nirmalraj <nirmalraj.joseph@twilightsoftwares.com>
2023-03-21 13:23:35 +0530

    Added Skills

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,38 +1,74 @@
 export const services = [
-    {
-        id:1,
-        name:'Web Development',
-        title:'Web Design & Logo',
-        description:
-        'Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.',
-    },
-    {
-        id:2,
-        name:'Web Development',
-        title:'Web Design & Logo',
-        description:
-        'Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.',
-
-    },
-    {
-        id:3,
-        name:'Web Development',
-        title:'Web Design & Logo',
-        description:
-        'Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.',
-    },
-    {
-        id:4,
-        name:'Web Development',
-        title:'Web Design & Logo',
-        description:
-        'Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.',
-        },
-        {
-    id:5,
-    name:'Web Development',
-    title:'Web Design & Logo',
+  {
+    id: 1,
+    name: "Web Development",
+    title: "Web Design & Logo",
     description:
-    'Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.',
-},
-]
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 2,
+    name: "Web Development",
+    title: "Web Design & Logo",
+    description:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 3,
+    name: "Web Development",
+    title: "Web Design & Logo",
+    description:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 4,
+    name: "Web Development",
+    title: "Web Design & Logo",
+    description:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 5,
+    name: "Web Development",
+    title: "Web Design & Logo",
+    description:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+];
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    percentage: 75,
+    decripition:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    percentage: 75,
+    decripition:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 3,
+    name: "JAVASCRIPT",
+    percentage: 75,
+    decripition:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 4,
+    name: "REACT",
+    percentage: 75,
+    decripition:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+  {
+    id: 5,
+    name: "ADOBE XD",
+    percentage: 75,
+    decripition:
+      "Web designer craft the overall vision & plan for a website layout .Professional logo development : Business,Company , or Personal.",
+  },
+];
